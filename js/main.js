3466dbc6cc05d91cfbe5cfbf1f9b15b6d97250b2
creastv <creastv@gmail.com>
2020-02-13 22:37:05 +0100

    commit

--- js/main.js
+++ js/main.js
@@ -1,1 +1,33 @@
-console.log('test');
+
+    var galleryThumbs = new Swiper('.gallery-thumbs', {
+      spaceBetween: 10,
+      slidesPerView: 3,
+      // loop: true,
+      // freeMode: true,
+      // loopedSlides: 3, //looped slides should be the same
+      // watchSlidesVisibility: true,
+      // watchSlidesProgress: true,
+    });
+    var galleryTop = new Swiper('.gallery-top', {
+      spaceBetween: 0,
+      effect: 'fade',
+      autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
+      // loop:true,
+      // loopedSlides: 5, //looped slides should be the same
+      thumbs: {
+        swiper: galleryThumbs,
+      },
+    });
+
+    var swiper = new Swiper('.swiper-wraper', {
+      direction: 'vertical',
+      slidesPerView: 'auto',
+      freeMode: true,
+      scrollbar: {
+        el: '.swiper-scrollbar',
+      },
+      mousewheel: true,
+    });
